6ab34e0eb43b267bae3d410dfe38270e0431bfeb
lufeng <lufeng@bingosoft.net>
2019-05-31 18:01:28 +0800

    初始化代码

---
+++ src/app.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="news">
+    <div class="header">
+      <span class="title">新闻</span>
+      <span class="more">更多</span>
+    </div>
+    <div class="content">
+      <div class="new-item" v-for="(item, $index) in items" :key="item">
+        <div class="photo-type" v-if="item.img">
+          <img :src="item.img"/>
+          <div class="right">
+            <p>{{item.title}}</p>
+            <span>{{item.showTime}}</span>
+          </div>
+        </div>
+        <div class="text-type" v-else>
+          <p>{{item.title}}</p>
+          <span>{{item.showTime}}</span>
+        </div>
+      </div>      
+    </div>
+  </div>
+</template>
+
+<script>
+import dateUtil from 'util/date'
+
+export default {
+  data () {
+    return {
+      items: [{
+        img: 'static/testImage/1.jpg',
+        title: '公有云厂商VS私有云厂商谁更强？',
+        url: 'https://www.baidu.com',
+        publishTime: 1553043600000
+      }, {
+        title: '个性需求接受or拒绝，揭秘产品组和项目组的三两事',
+        url: 'https://www.baidu.com',
+        publishTime: 1550797200000
+      }, {
+        title: '品高云入围Forrester报告，妥妥的',
+        url: 'https://www.baidu.com',
+        publishTime: 1550797200000
+      }, {
+        title: '公安行业部的行业云探索与实践',
+        url: 'https://www.baidu.com',
+        publishTime: 1550797200000
+      }]
+    }
+  },
+  components: {
+  },
+  created(){
+    for(var i = 0; i < this.items.length; i++){
+      this.items[i].showTime = dateUtil.simpleFormat(this.items[i].publishTime, 'MM-dd');
+    }
+  },
+  mounted(){
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~asset/common';
+@import '~asset/app';
+</style>
